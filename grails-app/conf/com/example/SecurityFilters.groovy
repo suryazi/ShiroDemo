29613e005604a68f9e97285ea389b46e4853fa9e
Suryazi Samiullah Khan <git@reseller.suryazi.com>
2014-08-17 16:50:12 +0300

    Assets included in grails-app/conf/com/example/SecurityFilters.groovy

--- grails-app/conf/com/example/SecurityFilters.groovy
+++ grails-app/conf/com/example/SecurityFilters.groovy
@@ -5,13 +5,30 @@ package com.example
  * via access control by convention.
  */
 class SecurityFilters {
+    
+    def publicActions = [
+        signup: ['index','register'],
+        auth:['*'],
+        assets:['*']
+    ]
+    
+    private boolean findAction(controllerName, actionName){
+        def c = publicActions[controllerName]
+        return(c)?c.find{(it==actionName||it=='*')}!=null:false
+    }
+    
     def filters = {
-        all(uri: "/**") {
+        
+        all(uri: "/**"){
             before = {
+                //Check for public controller/actions
+                def isPublic=findAction(controllerName,actionName)
+                
+                if(isPublic) return true
+                
                 // Ignore direct views (e.g. the default main index page).
                 if (!controllerName) return true
-
-                // Access control by convention.
+                
                 accessControl()
             }
         }
