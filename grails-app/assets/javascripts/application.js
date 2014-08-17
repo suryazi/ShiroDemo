890334368617668bf2ccbb460e8296588d293bd6
Suryazi Samiullah Khan <git@reseller.suryazi.com>
2014-08-17 09:29:48 +0300

    Upgraded Assest pipleline plugin and added bootstrap and jquery.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,7 +5,8 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require jquery
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.js
 //= require_tree .
 //= require_self
 
